d1cff61db487b38a62a80c77976286db50dff146
Lizbeth <liz.loang@gmail.com>
2023-01-11 23:31:52 -0600

    initial config (docker,prisma,tailwind)

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const draga = await prisma.draga.findUnique({
+    where: {
+      id: 1,
+    },
+  });
+
+  console.log(draga);
+  res.status(200).json(draga);
 }
---
+++ prisma/migrations/20230111064849_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Draga" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "Draga_pkey" PRIMARY KEY ("id")
+);
